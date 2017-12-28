fb94003077c0e536b6e5ed526babf5affee24db6
Ashish Upadhyay <ashishupadhyay855@gmail.com>
2017-12-28 22:35:29 +0530

    nnn

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
 const express = require('express');
 const app = express();
+const mongoose=require('mongoose');
+const config =require('./config/database');
+const path = require('path');
 
-app.get('/',(req,res)=>{
-    res.send('hello world');
+mongoose.Promise = global.Promise;
+mongoose.connect( config.uri,(err)=>{
+    if (err){
+        console.log('could not conect to database:',err);
+    }else{
+        console.log(config.secret);
+        console.log('connected to database: ' + config.db );
+    }
+});
+
+// app.use(express.static(__dirname +'/client/src/'));
+// app.get('*',(req,res)=>{
+// res.sendFile(path.join(__dirname + '/client/src/index.html'))
+// })
+  
+
+app.get('*',(req,res)=>{
+    res.send('<h1>hello world');
 });
 
 
