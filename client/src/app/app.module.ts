3394b5e13706fe788f59c283420ebfc79da4f7c4
Ashish Upadhyay <ashishupadhyay855@gmail.com>
2018-01-03 22:06:39 +0530

    register

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,10 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';    
 import { NgModule } from '@angular/core';
-<<<<<<< HEAD
 import{ReactiveFormsModule} from '@angular/forms';
-=======
-import{ ReactiveFormsModule }  from '@angular/forms';
->>>>>>> 7710718f9df1b9949e49ac635024571705968e0e
 import{HttpModule} from '@angular/http';
 import{AppRoutingModule} from './app-routing.module';
 import { AppComponent } from './app.component';
