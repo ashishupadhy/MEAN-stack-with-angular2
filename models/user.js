35a713d66749043979d3cf886ebc5134bf35ee85
Ashish Upadhyay <ashishupadhyay855@gmail.com>
2018-01-09 14:10:27 +0530

    compare password added

--- models/user.js
+++ models/user.js
@@ -145,7 +145,9 @@ userSchema.pre('save', function (next) {
     });
 });
 
-userSchema.methods.comparePassword = (password) => {
+    
+
+userSchema.methods.comparePassword = function(password)  {
     return bcrypt.compareSync(password, this.password);
 };
 
