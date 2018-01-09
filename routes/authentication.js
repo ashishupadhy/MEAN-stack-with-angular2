5eb74e7f4c9be03e65c2a81068c55c1ca085decb
Ashish Upadhyay <ashishupadhyay855@gmail.com>
2018-01-09 14:11:39 +0530

    token get added

--- routes/authentication.js
+++ routes/authentication.js
@@ -1,4 +1,10 @@
 const User = require('../models/user');
+const jwt = require('jsonwebtoken');
+const config = require('../config/database');   
+
+
+
+
 
 module.exports = (router) => {
 
@@ -36,17 +42,19 @@ module.exports = (router) => {
 
                                     if (err.errors.email) {
                                         res.json({ success: false, message: err.errors.email.message });
-                                    }else{
-                                        if(err.errors.username){
-                                             res.json({ success: false, message: err.errors.username.message 
+                                    } else {
+                                        if (err.errors.username) {
+                                            res.json({
+                                                success: false, message: err.errors.username.message
                                             });
-                                        }else{
-                                            if(err.errors.password){
-                                                res.json({ success: false, message: 
-                                                    err.errors.password.message 
-                                            });
-                                            }else{
-                                                res.json({success:false, message:err});
+                                        } else {
+                                            if (err.errors.password) {
+                                                res.json({
+                                                    success: false, message:
+                                                    err.errors.password.message
+                                                });
+                                            } else {
+                                                res.json({ success: false, message: err });
                                             }
                                         }
                                     }
@@ -57,62 +65,93 @@ module.exports = (router) => {
                                     });
                                 }
 
-                            
-                        
-
-                        // res.json({
-                        //     success: false, message: 'could not save user.Error:', err
-                        // });
 
 
-                    }  
+
+                                // res.json({
+                                //     success: false, message: 'could not save user.Error:', err
+                                // });
+
+
+                            }
 
                         } else {
-                    res.json({ success: true, message: 'Account Registerd !' });
+                            res.json({ success: true, message: 'Account Registerd !' });
+                        }
+
+                    });
                 }
-    
-            });
-}
-                } 
+            }
         }
     });
-    router.get('/checkEmail/:email',(req, res) => {
-        if(!req.params.email) {
-            res.json({success: false, message: 'E-mail was not provided'});
+    router.get('/checkEmail/:email', (req, res) => {
+        if (!req.params.email) {
+            res.json({ success: false, message: 'E-mail was not provided' });
 
-        }else{
-            User.findOne({email: req.params.email},(err,user)=>{
-                if (err){
-                    res.json({success: false, message:err});
-                }else{
-                    if(user){
-                        res.json({success: false, message:'E-mail is already taken'});
-                    }else{
-                        res.json({success: true, message:'E-mail is available'});
+        } else {
+            User.findOne({ email: req.params.email }, (err, user) => {
+                if (err) {
+                    res.json({ success: false, message: err });
+                } else {
+                    if (user) {
+                        res.json({ success: false, message: 'E-mail is already taken' });
+                    } else {
+                        res.json({ success: true, message: 'E-mail is available' });
                     }
                 }
             });
         }
     });
 
-    router.get('/checkUsername/:username',(req, res) => {
+    router.get('/checkUsername/:username', (req, res) => {
         if (!req.params.username) {
-            res.json({success: false, message:'Username was not provided'});
+            res.json({ success: false, message: 'Username was not provided' });
 
-        }else{
-            User.findOne({ username: req.params.username},(err, user)=>{
-                if (err){
-                    res.json({success: false, message: err});
-                }else{
-                    if(user) {
-                        res.json({ success: false, message: 'Username is already taken'});
-                    }else{
-                        res.json({success: true, message: 'Username is available'})
+        } else {
+            User.findOne({ username: req.params.username }, (err, user) => {
+                if (err) {
+                    res.json({ success: false, message: err });
+                } else {
+                    if (user) {
+                        res.json({ success: false, message: 'Username is already taken' });
+                    } else {
+                        res.json({ success: true, message: 'Username is available' });
                     }
                 }
             });
         }
     });
 
-return router;
+
+    router.post('/login', (req, res) => {
+        if (!req.body.username) {
+            res.json({ success: false, message: 'No username was provided' });
+        } else {
+            if (!req.body.password) {
+                res.json({ success: false, message: 'No password provided.' });
+            } else {
+
+                
+                User.findOne({ username: req.body.username.toLowerCase() }, (err, user) => {
+                    if (err) {
+                        res.json({ success: false, message: err });
+                    } else {
+                        if (!user) {
+                            res.json({ success: false, message: 'Username not found.' });
+                        } else {
+                            const validPassword = user.comparePassword(req.body.password);
+                            if (!validPassword) {
+                                res.json({ success: false, message: 'Password invalid' });
+                            } else {
+                            const token = jwt.sign({ userId: user._id}, config.secret, {expiresIn:'24h'});
+                            res.json({ success: true, message: 'Success!',token: token, user: {username: user.username} });
+                            }
+                        }
+                    }
+                })
+            }
+        }
+    });
+
+    return router;
 }
