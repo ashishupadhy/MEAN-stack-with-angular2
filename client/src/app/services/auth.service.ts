557dc65b57c002892d679c446f90653dbb9474d4
Ashish Upadhyay <ashishupadhyay855@gmail.com>
2018-01-10 16:33:02 +0530

    service added

--- client/src/app/services/auth.service.ts
+++ client/src/app/services/auth.service.ts
@@ -1,12 +1,14 @@
-import { Injectable } from '@angular/core';
+  import { Injectable } from '@angular/core';
 import { Http, Headers, RequestOptions } from '@angular/http';
 import 'rxjs/add/operator/map';
 @Injectable()
 export class AuthService {
 
-domain="http://localhost:8080"
+domain="http://localhost:8080";
+authToken;
+user;
 
-  constructor
+  constructor 
   (  private http:Http) { }                    
 
 registerUser(user) {
@@ -20,7 +22,16 @@ checkUsername(username) {
 checkEmail(email) {
   return this.http.get(this.domain +'/authentication/checkEmail/'+ email).map(res=>res.json());
 }
-    
+ 
+login(user){
+  return this.http.post(this.domain + '/authentication/login',user).map(res =>res.json())
+}
+storeUserData(token,user){
+  localStorage.setItem('token',token);
+  localStorage.setItem('user',JSON.stringify(user));
+  this.authToken = token;
+  this.user = user;
+}
   
 }
   
