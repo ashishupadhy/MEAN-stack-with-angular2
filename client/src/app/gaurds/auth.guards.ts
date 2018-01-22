8d95803feb98a425bb35b449f4a539557e504309
Ashish Upadhyay <ashishupadhyay855@gmail.com>
2018-01-22 15:44:32 +0530

    auth guards added

--- client/src/app/gaurds/auth.guards.ts
+++ client/src/app/gaurds/auth.guards.ts
@@ -1,11 +1,20 @@
-// import { Injectable }       from '@angular/core';
-// import {CanActivate, Router,ActivatedRouteSnapshot, RouterStateSnapshot} from '@angular/router';                           from '@angular/router';
-// // import { AuthService }      from './auth.service';
+import { Injectable }       from '@angular/core';
+import {CanActivate, Router,ActivatedRouteSnapshot, RouterStateSnapshot} from '@angular/router';  
+import { AuthService }      from '../services/auth.service';
 
-// @Injectable()
-// export class AuthGuard implements CanActivate {
-//   canActivate() {
-//     console.log('AuthGuard#canActivate called');
-//     return true;
-//   }
-// }
+@Injectable()
+export class AuthGuard implements CanActivate {
+    constructor 
+  (  private authService: AuthService,
+    private router:Router
+) { }   
+  
+   canActivate() {
+   if(this.authService.loggedIn()){
+       return true;
+   }else{
+       this.router.navigate(['/login']);
+       return  false;
+   }
+  }
+} 
